619ff1fd2ecab375df5cdc7f23897d3662da9614
Yash Gupta <yashhguptaa1@gmail.com>
2021-12-09 09:18:50 +0530

    Injecting repo using services

---
+++ src/main/java/com/yg/SpringMicroservices/service/TourPackageService.java
@@ -0,0 +1,42 @@
+package com.yg.SpringMicroservices.service;
+
+import com.yg.SpringMicroservices.domain.TourPackage;
+import com.yg.SpringMicroservices.repo.TourPackageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TourPackageService {
+    private TourPackageRepository tourPackageRepository;
+
+    @Autowired
+    public TourPackageService(TourPackageRepository tourPackageRepository) {
+        this.tourPackageRepository = tourPackageRepository;
+    }
+
+    /**
+     * Create a Tour Package
+     *
+     * @param code code of the package
+     * @param name name of the package
+     *
+     * @return new or existing tour package
+     */
+    public TourPackage createTourPackage(String code, String name) {
+        return tourPackageRepository.findById(code).orElse(tourPackageRepository.save(new TourPackage(code, name)));
+    }
+
+    /**
+     * Lookup All Tour packages
+     *
+     * @return
+     */
+    public Iterable<TourPackage> lookup(){
+        return tourPackageRepository.findAll();
+    }
+
+    public long total() {
+        return tourPackageRepository.count();
+    }
+}
+
